4ff55d979912fd3d47853af03b76f29d60e7046a
NicolasMatiasWW <nmatias@warren-walter.com>
2017-03-01 14:48:32 +0100

    Test commit 2
    azf

--- studioactions/tablet/AS_Button_1ab0937998ea4dac8ea447ceec2b5e33.js
+++ studioactions/tablet/AS_Button_1ab0937998ea4dac8ea447ceec2b5e33.js
@@ -1,12 +1,14 @@
 function AS_Button_1ab0937998ea4dac8ea447ceec2b5e33(eventobject) {
     function TRANSFORM_ACTION____dde7d92f93bd4c7ba93c89c179b5c7fd_Callback() {}
     var trans100 = kony.ui.makeAffineTransform();
-    Form0798d8cd187534d.Button0917fc334bcb14d.animate(kony.ui.createAnimation({
+    Form0798d8cd187534d.Button0917fc334bcb14d.animate(
+    kony.ui.createAnimation({
         "100": {
             "stepConfig": {
                 "timingFunction": kony.anim.EASE
             },
             "width": "2dp",
+            "right": "54dp",
             "transform": trans100
         }
     }), {
